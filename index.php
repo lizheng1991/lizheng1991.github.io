767d8ec21366e6159e7d9d4ce0ebff687d06504d
lizheng1991 <lizheng_0508@163.com>
2016-10-25 22:45:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
-echo header("Access-Control-Allow-Origin:*"); 
+header("Access-Control-Allow-Origin:*"); 
+header('Access-Control-Allow-Methods:GET'); 
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 $username=$_GET['username'];
 $password=$_GET['password'];
 $staff = array(
