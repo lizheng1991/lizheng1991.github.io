53d97c1bda3f4c5402cca72390261d877bb2519b
lizheng1991 <lizheng_0508@163.com>
2017-04-25 10:41:38 +0800

    Revert "Revert "Update index.php""
    This reverts commit be42f615e6a077462b810480c1bdd36239a5ef22.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
-header("Access-Control-Allow-Origin:*"); 
-header('Access-Control-Allow-Methods:GET'); 
-header('Access-Control-Allow-Headers:x-requested-with,content-type');
+//header("Access-Control-Allow-Origin:*"); 
+//header('Access-Control-Allow-Methods:GET'); 
+//header('Access-Control-Allow-Headers:x-requested-with,content-type');
 $username=$_GET['username'];
 $password=$_GET['password'];
 $staff = array(
